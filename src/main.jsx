9ae4624922ddf36673a92b1bae5f1d025360c951
SamuelLJG <samuelljg@outlook.com>
2024-04-09 20:26:33 -0300

    tranfer to react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Chiori from './pages/Chiori';
+import Baizhu from './pages/Baizhu';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Sobre from './pages/Sobre';
+import ErrorPage from './pages/ErrorPage';
+import { HelmetProvider } from 'react-helmet-async';
+
+const router = createBrowserRouter([
+  {
+    path: "/genshin-react",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/genshin-react",
+        element: <Home />,
+      },
+      {
+        path: "/genshin-react/contato",
+        element: <Contato />,
+      },
+      {
+        path: "/genshin-react/sobre",
+        element: <Sobre />,
+      },
+      {
+        path: "/genshin-react/chiori",
+        element: <Chiori />,
+      },
+      {
+        path: "/genshin-react/baizhu",
+        element: <Baizhu />,
+      }
+    ]
+  },
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HelmetProvider>
+    <RouterProvider router={router} />
+    </HelmetProvider>
+  </React.StrictMode>
+);
+
