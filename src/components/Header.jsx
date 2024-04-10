a9a47c0e68d2c167ab74841bb0f005f25f8b620f
SamuelLJG <samuelljg@outlook.com>
2024-04-10 00:34:49 -0300

    genshin_react added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/contato'>Contato</Link>
-        <Link to='/sobre'>Sobre</Link>
-        <Link to='/chiori'>Chiori</Link>
-        <Link to='/baizhu'>Baizhu</Link>
+        <Link to='/genshin_react/'>Home</Link>
+        <Link to='/genshin_react/contato'>Contato</Link>
+        <Link to='/genshin_react/sobre'>Sobre</Link>
+        <Link to='/genshin_react/chiori'>Chiori</Link>
+        <Link to='/genshin_react/baizhu'>Baizhu</Link>
     </nav>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -13,28 +13,28 @@ import { HelmetProvider } from "react-helmet-async"
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/genshin_react/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/",
+        path: "/genshin_react/",
         element: <Home />,
       },
       {
-        path: "/contato",
+        path: "/genshin_react/contato",
         element: <Contato />,
       },
       {
-        path: "/sobre",
+        path: "/genshin_react/sobre",
         element: <Sobre />,
       },
       {
-        path: "/chiori",
+        path: "/genshin_react/chiori",
         element: <Chiori />,
       },
       {
-        path: "/baizhu",
+        path: "/genshin_react/baizhu",
         element: <Baizhu />,
       }
     ]
