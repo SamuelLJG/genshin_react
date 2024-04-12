6c6924f3a6ebf3a0ebf548dc1035d8f9a85196c8
SamuelLJG <samuelljg@outlook.com>
2024-04-12 06:18:03 -0300

    header half complete, characters page half complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,14 @@
 import {Link} from 'react-router-dom'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const Header = () => {
   return <header>
     <nav id='pc'>
         <div id='nav-cover'>
-          <Link to='/' className='home-link'>GENSHIN&nbsp;<i class="fa-brands fa-meta"></i>&nbsp;BUILD</Link>
+          <Link to='/' className='home-link header-hover'>GENSHIN&nbsp;<i class="fa-brands fa-meta"></i>&nbsp;BUILD</Link>
           <div id='pc-nav'>
 
-            <Link to='/' id='characters-pop-up'><i class="fa-solid fa-image-portrait"></i>&nbsp;Personagens
-              <div id='pop-up-list'>
-                <div>ANEMO<i class="fa-solid fa-caret-right sieta" aria-hidden="true"></i></div>
-                <div>CRYO<i class="fa-solid fa-caret-right sieta" aria-hidden="true"></i></div>
-                <div>DENDRO<i class="fa-solid fa-caret-right sieta" aria-hidden="true"></i></div>
-                <div>ELETRO<i class="fa-solid fa-caret-right sieta" aria-hidden="true"></i></div>
-                <div>GEO<i class="fa-solid fa-caret-right sieta" aria-hidden="true"></i></div>
-                <div>HYDRO<i class="fa-solid fa-caret-right sieta" aria-hidden="true"></i></div>
-                <div>PYRO<i class="fa-solid fa-caret-right sieta" aria-hidden="true"></i></div>
-              </div>
-            </Link>
-
+            <Link to='/'><i class="fa-solid fa-image-portrait"></i>&nbsp;Personagens</Link>
             <Link to='/tier-list'><i class="fa-solid fa-people-group"></i>&nbsp;Tier-List</Link>
             <Link to='/times'><i class="fa-solid fa-users"></i>&nbsp;Times</Link>
             <Link to='/farming'><i class="fa-solid fa-wheat-awn" ></i>&nbsp;Farming</Link>
--- src/main.jsx
+++ src/main.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
       {
         path: "/chiori",
         element: <Chiori />,
-      }
+      },
     ]
   },
   
